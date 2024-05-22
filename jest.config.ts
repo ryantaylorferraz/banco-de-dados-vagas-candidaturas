8e550cde4284ca0f40b1d58d5eea64f498f93e5e
ryan <ryan.bilac2010@hotmail.com>
2024-05-22 12:42:30 -0300

    installation library jest

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ["**/_tests_/(units|integrations)/**/*.[jt]s?(X)"]
+};
